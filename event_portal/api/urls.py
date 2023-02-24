15832e29d8bcfbe3dd863a8128a34bb7871a3451
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-02-25 00:20:19 +0100

    created Category List, Category Create, Category Detail, Event Create and Event Detail Views

---
+++ event_portal/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views 
+
+
+urlpatterns = [
+    path("category-list/", views.CategoryListView.as_view(), name="category-list"),
+    path("create-category/", views.CategoryCreateView.as_view(), name="create-category"),
+    path("category-detail/<slug:slug>/", views.CategoryDetailView.as_view(), name="category-detail"),
+    path("create-event/", views.CreateEventView.as_view(), name="create-event"), 
+    path("event-details/<slug:slug>/", views.EventDetailView.as_view(), name="event-details")
+]
