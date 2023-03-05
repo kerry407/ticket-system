ac109ff84dcf148383159a808e5609f29322d2de
Moses Nwigberi <mosesnwigberi@gmail.com>
2023-03-05 11:40:47 +0100

    Created Hero section

--- client/components/Hero/Hero.jsx
+++ client/components/Hero/Hero.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import Image from 'next/image'
+import styles from './Hero.module.css'
+import bg1 from '../../public/Images/bg-1.jpeg'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className={styles.Hero}>
+      <header>
+        <div><h1>Get-Ticketz</h1></div>
+        <div>
+          <ul>
+            <li>Home</li>
+            <li>Pages</li>
+            <li>Agenda</li>
+            <li>Speakers</li>
+            <li>Blog</li>
+          </ul>
+        </div>
+        <div className={styles.ticks}>
+          <div></div>
+          <div>Buy Tickets</div>
+        </div>
+      </header>
+      <Image src={bg1} layout="fill" alt='Hero-Image' />
+      <div className={styles.overlay}></div>
+      <div className={styles.banner}>
+        <div>
+          <div className={styles.date}>
+            <div>23</div>
+            <div>05</div>
+            <div>MAY<br /></div> 
+            <div>2023</div>
+          </div>
+        </div>
+        <div>
+          <h2>PRESENTING...</h2>
+          <h1><span>#1</span> Virtual Video
+            <br />Marketing <span>Conference.</span>
+          </h1>
+          <button>Get Tickets</button><button className={styles.learn}>Learn More</button> 
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client/pages/index.js
+++ client/pages/index.js
@@ -4,6 +4,7 @@ import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 import Hero from '../components/Hero/Hero'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
