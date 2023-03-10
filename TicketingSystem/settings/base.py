670624bae14bf5de708e8c43fa8089c8ca83f3ab
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-03-11 00:53:14 +0100

    Properly configured whitenoise

--- TicketingSystem/settings/base.py
+++ TicketingSystem/settings/base.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 
 MEDIA_URL = "/media/"
