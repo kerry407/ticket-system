d83409da2243338e932ef3c83ca78e61a676d254
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-03-11 18:36:47 +0100

    Added an extra permission

--- event_portal/api/permissions.py
+++ event_portal/api/permissions.py
@@ -1,5 +1,12 @@
 from rest_framework import permissions 
 
+
+class EventHostPermissions(permissions.BasePermission):
+    
+    def has_permission(self, request, view):
+        return request.user.event_hoster
+
+
 class EventHostORReadOnly(permissions.BasePermission):
     
     def has_object_permission(self, request, view, obj):
