a60d709668d84bd83852c50ccefca17ed287a483
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-10-20 15:59:55 +0100

    ready for production

--- TicketingSystem/settings/base.py
+++ TicketingSystem/settings/base.py
@@ -79,6 +79,13 @@ WSGI_APPLICATION = 'TicketingSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
