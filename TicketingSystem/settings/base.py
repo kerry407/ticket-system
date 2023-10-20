446d5dde7edd069e97b30e23c4b4ba379a486644
Kerry Onyeogo <kerryonyeogo@gmail.com>
2023-10-20 15:59:04 +0100

    ready for production

--- TicketingSystem/settings/base.py
+++ TicketingSystem/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -138,6 +139,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
--- TicketingSystem/settings/prod.py
+++ TicketingSystem/settings/prod.py
@@ -6,14 +6,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["eventmanagement-y16a.onrender.com"]
 
-# Whitenoise settings for static files in production 
-MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
-# Render PostgreSQL live database
 
-# DATABASES = {
-#     'default': dj_database_url.parse(config("DATABASE_URL"))
-# }
